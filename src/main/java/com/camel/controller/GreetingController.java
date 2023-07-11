1de9d40ba05ee1c91f86f0e8692f690be39dc6e2
abhin <abhin@MAA>
2023-07-11 21:05:27 +0530

    d

--- src/main/java/com/camel/controller/GreetingController.java
+++ src/main/java/com/camel/controller/GreetingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @Autowired
+    
     private ProducerTemplate producerTemplate;
 
     @PostMapping("/greet")
